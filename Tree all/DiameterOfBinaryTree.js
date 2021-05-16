9e92e6c9a159c83e23e713fcf163eb13cf382da0
Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>
2021-05-16 11:50:08 +0530

    Update DiameterOfBinaryTree.js
    updated with better solution

--- Tree all/DiameterOfBinaryTree.js
+++ Tree all/DiameterOfBinaryTree.js
@@ -13,6 +13,10 @@ function getHeight (root) {
     return 1 + Math.max(getHeight(root.left),getHeight(root.right));
 }
 
+//base solution
+//complexity - o(n * h)
+//h == logn --> o(nlogn)
+//h == n --> o(n^2)
 function getDiameter(root){
     if(root == null)
         return 0;
@@ -21,10 +25,37 @@ function getDiameter(root){
     let option3 = getDiameter(root.right);
     return Math.max(option1, Math.max(option2, option3));
 }
+
+//better solution
+//complexity - o(n * h)
+//h == logn --> o(nlogn)
+//h == n --> o(n)
+function getHeightAndDiameter(root) {
+  if(root == null){
+    return{
+      height:0,
+      diameter:0
+    }
+  }
+  let lo = getHeightAndDiameter(root.left);
+  let ro = getHeightAndDiameter(root.right);
+
+  let height = 1 + Math.max(lo.height, ro.height);
+
+  let option1 = lo.height + ro.height;
+  let option2 = lo.diameter;
+  let option3 = ro.diameter;
+  let diameter = Math.max(option1,Math.max(option2,option3));
+  return {
+    height,
+    diameter
+  }
+}
+
 (function main() {
     let root = new Node(1);
     root.left = new Node(2)    
-    // root.right = new Node(3)
+    root.right = new Node(3)
     
     root.left.left = new Node(4);
     root.left.right = new Node(5);
@@ -32,5 +63,5 @@ function getDiameter(root){
     root.left.left.left = new Node(6);
     root.left.left.right = new Node(7);
 
-    console.log(getDiameter(root));
+    console.log(getHeightAndDiameter(root).diameter);
 }());
